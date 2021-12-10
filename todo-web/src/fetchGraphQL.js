51a6ffff365c04976dfa01d06dfc8aafd005384f
Shawn Zhao <szhao2@flexport.com>
2021-12-10 11:42:18 +0800

    amend appearance

---
+++ todo-web/src/fetchGraphQL.js
@@ -0,0 +1,17 @@
+async function fetchGraphQL(text, variables) {
+    // fetch data from the rails server
+    const response  = await fetch('http://localhost:3000/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query: text,
+            variables,
+        }),
+    });
+
+    return await response.json();
+}
+
+export default fetchGraphQL;
